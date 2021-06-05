3404a1fce11e315aef539195a21bd7d20c245596
Raunak <raunaksomani321@gmail.com>
2021-06-05 11:42:55 +0530

    final commit for heroku

--- Bus_Prix/settings.py
+++ Bus_Prix/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -42,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,4 +127,5 @@ STATIC_URL = '/static/'
 # managing media
 import os
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Bus_Prix.wsgi
